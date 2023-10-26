8280eb0a2d39fa6ec147492e1faf70215cff1641
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2023-10-26 20:55:47 +0000

    update react folder for more functionalities

--- webpack.config.js
+++ webpack.config.js
@@ -11,4 +11,26 @@ module.exports = {
   plugins: [
     new CopyWebpackPlugin(['index.html'])
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  },
+  experiments: {
+    syncWebAssembly: true,
+    asyncWebAssembly: true,
+  }
 };
